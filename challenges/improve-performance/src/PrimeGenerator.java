d421f088e5b6462b72409fa0b0f5fd2f81016c89
vppmatt <matt@multicode.co.uk>
2025-05-05 12:59:37 +0100

    simplifying prime generator

--- challenges/improve-performance/src/PrimeGenerator.java
+++ challenges/improve-performance/src/PrimeGenerator.java
@@ -1,9 +1,7 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class PrimeGenerator {
 
@@ -11,31 +9,13 @@ public class PrimeGenerator {
 
         System.out.println("About to find " + size + " primes.");
 
-        ConcurrentLinkedQueue<BigInteger> primes = new ConcurrentLinkedQueue<>();
-        ForkJoinPool customThreadPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
-
-        try {
-            customThreadPool.submit(() -> {
-                while (primes.size() < size) {
-                    int remaining = size - primes.size();
-                    ThreadLocalRandom.current()
-                        .ints(remaining * 2) // Generate more candidates dynamically
-                        .parallel()
-                        .mapToObj(i -> new BigInteger(2000, ThreadLocalRandom.current()).nextProbablePrime())
-                        .forEach(prime -> {
-                            if (primes.size() < size) {
-                                primes.add(prime);
-                            }
-                        });
-                }
-            }).get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            customThreadPool.shutdown();
+       List<BigInteger> primes = new ArrayList<>();
+        
+        for (int i = 0; i < size; i++) {
+            primes.add(new BigInteger(2000, new Random()).nextProbablePrime());
         }
 
         System.out.println("Found all " + primes.size() + " primes.");
-        return new ArrayList<>(primes).subList(0, size);
+        return primes;          
     }
 }
